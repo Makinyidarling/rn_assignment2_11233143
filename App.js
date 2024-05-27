2cb6efd5a75406f3567d6a1b503273d308c20e14
makinyidarling <gwulimaja@st.ug.edu.gh>
2024-05-27 12:24:28 +0000

    commiting assingment two

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>My name is <Text style={styles.name}>Godwin Wulimaja</Text></Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'yellow',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  name: {
+    fontWeight:"bold",
+  }
+});
---
+++ README.md
@@ -0,0 +1,10 @@
+![alt text](<WhatsApp Image 2024-05-27 at 11.57.35_d376b8ae.jpg>)
+# Assingment2
+## Task one 
+An app was created and the background color of the app was changed.
+### Task two 
+The text in the background was changed to my name.
+#### Task four 
+The font size of the text was increased to 24.
+##### Task five
+My name was made bold.
